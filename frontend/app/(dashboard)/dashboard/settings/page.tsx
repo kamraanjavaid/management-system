6577ed7b819d89608e2e427ca70b46930191c846
dev4-marketasa <khaankamran.121@gmail.com>
2026-02-08 23:35:27 +0400

    settings and customer page

---
+++ frontend/app/(dashboard)/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardSettingsPage() {
+  return (
+    <div className="p-6 space-y-6 bg-gray-50 min-h-screen">
+      <p className="text-gray-700">Sorry for inconvience this part is under development...</p>
+      {/* Future settings components will go here */}
+    </div>
+  );
+}
